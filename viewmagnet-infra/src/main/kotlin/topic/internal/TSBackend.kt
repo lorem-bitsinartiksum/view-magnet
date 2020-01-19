49ab0896234d8a36276a3762e951caebdfa57c2c
cakir-enes <caki-enes@outlook.com>
2020-01-19 18:40:20 +0300

    add serde and topic serv backend interfaces

--- viewmagnet-infra/src/main/kotlin/topic/internal/TSBackend.kt
+++ viewmagnet-infra/src/main/kotlin/topic/internal/TSBackend.kt
@@ -1,10 +1,11 @@
 package topic.internal
 
 import kotlinx.coroutines.channels.ReceiveChannel
+import topic.TopicContext
 
 internal interface TSBackend {
 
-    fun publish(name: String, payload: ByteArray)
+    fun publish(name: String, context: TopicContext, payload: ByteArray)
 
-    fun subscribe(name: String): ReceiveChannel<ByteArray>
+    fun subscribe(name: String, context: TopicContext): ReceiveChannel<ByteArray>
 }
