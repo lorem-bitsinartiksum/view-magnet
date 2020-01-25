e8e9bf691f25469d6f3d29c22e2968b7cc5dacf3
Alper Öner <alperoner0710@gmail.com>
2020-01-25 20:57:41 +0300

    add javalin dependency and related dependencies

---
+++ viewmagnet-web/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val app = Javalin.create().start(7000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
+}
