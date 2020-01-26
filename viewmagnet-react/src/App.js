92ae6265aea43dcc09a4fadb5e004dcae3d947ec
bur-k <b.koc@etu.edu.tr>
2020-01-26 14:18:34 +0300

    implement register

--- viewmagnet-react/src/App.js
+++ viewmagnet-react/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Redirect to="/" />
+        </Switch>
     </div>
   );
 }
---
+++ viewmagnet-react/src/components/RegisterForm.js
@@ -0,0 +1,109 @@
+import { Form, Icon, Input, Button, Card } from 'antd';
+import React from 'react';
+import axios from 'axios'
+import 'antd/dist/antd.css';
+import './RegisterForm.css';
+
+class NormalRegisterForm extends React.Component {
+    state = {
+        confirmDirty: false,
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                axios.post('https://bububu.free.beeceptor.com', values).then(function (response) {
+                    console.log(response)
+                }).catch(function (error) {
+                    console.log(error);
+                })
+            }
+        });
+    };
+
+    handleConfirmBlur = e => {
+        const { value } = e.target;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Card className="register-card">
+                <Form onSubmit={this.handleSubmit} className="register-form">
+                    <Form.Item>
+                        {getFieldDecorator('email', {
+                            rules: [{
+                                type: 'email',
+                                message: 'The input is not valid Email!',
+                            },
+                            {
+                                required: true,
+                                message: 'Please input your Email!',
+                            }],
+                        })(
+                            <Input
+                                prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="Email"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        {getFieldDecorator('password', {
+                            rules: [
+                                { required: true, message: 'Please input your Password!' },
+                                { validator: this.validateToNextPassword }
+                            ],
+                        })(
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="Password"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        {getFieldDecorator('confirm', {
+                            rules: [
+                                {
+                                    required: true,
+                                    message: 'Please confirm your password!',
+                                },
+                                {
+                                    validator: this.compareToFirstPassword,
+                                },
+                            ],
+                        })(<Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="Re-type your password" onBlur={this.handleConfirmBlur}/>)}
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="register-form-button">Register</Button>Or <a href="">log in now!</a>
+                    </Form.Item>
+                </Form>
+            </Card>
+        );
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('Two passwords that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    };
+
+    validateToNextPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    };
+}
+
+const RegisterForm = Form.create({ name: 'normal_register' })(NormalRegisterForm);
+export default RegisterForm;
