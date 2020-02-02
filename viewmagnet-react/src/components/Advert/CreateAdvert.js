304c8a9803ebb5a33e51320e277b98a47ccc3010
bur-k <b.koc@etu.edu.tr>
2020-02-03 01:22:55 +0300

    implement form field onChanges

--- viewmagnet-react/src/components/Advert/CreateAdvert.js
+++ viewmagnet-react/src/components/Advert/CreateAdvert.js
@@ -5,15 +5,29 @@ const { Option } = Select;
 const { TextArea } = Input;
 
 class CreateAdvert extends React.Component {
+    state = {
+        title: "",
+        description: "",
+        ageRange: "",
+        gender: "",
+        weather: "",
+        poster: "",
+    }
+    onChangeTitle = str => this.setState({ title: str.target.value });
+    onChangeDesc = str => this.setState({ description: str.target.value });
+    onChangeAgeRange = str => this.setState({ ageRange: str.target.value });
+    onChangeGender = str => this.setState({ gender: str.target.value });
+    onChangeWeather = str => this.setState({ weather: str });
+
     render() {
         return (
             <Card className="advert-card">
                 <Form>
                     <Form.Item label="Advert Title">
-                        <Input />
+                        <Input onChange={this.onChangeTitle} />
                     </Form.Item>
                     <Form.Item label="Target Age Range">
-                        <Radio.Group>
+                        <Radio.Group onChange={this.onChangeAgeRange} >
                             <Radio.Button value="BABY">Baby</Radio.Button>
                             <Radio.Button value="CHILD">Child</Radio.Button>
                             <Radio.Button value="YOUNG">Young</Radio.Button>
@@ -22,14 +36,14 @@ class CreateAdvert extends React.Component {
                         </Radio.Group>
                     </Form.Item>
                     <Form.Item label="Target Gender">
-                        <Radio.Group>
+                        <Radio.Group onChange={this.onChangeGender}>
                             <Radio.Button value="MALE">Male</Radio.Button>
                             <Radio.Button value="FEMALE">Female</Radio.Button>
                             <Radio.Button value="UNDETECTED">Undetected</Radio.Button>
                         </Radio.Group>
                     </Form.Item>
                     <Form.Item label="Target Weather" >
-                        <Select placeholder="Please select target weather">
+                        <Select placeholder="Please select target weather" onChange={this.onChangeWeather}>
                             <Option value="SUNNY">Sunny</Option>
                             <Option value="CLOUDY">Cloudy</Option>
                             <Option value="WINDY">Windy</Option>
@@ -41,20 +55,24 @@ class CreateAdvert extends React.Component {
                         </Select>
                     </Form.Item>
                     <Form.Item label="Upload Advert Poster">
-                        <Upload name="logo" action="/upload.do" listType="picture">
+                        <Upload listType="picture" beforeUpload={(f) => {
+                            let reader = new FileReader();
+                            reader.readAsDataURL(f);
+                            reader.onloadend = () => { this.setState({ poster: reader.result }); return false; };
+                        }}>
                             <Button>
                                 <Icon type="upload" />Click to upload
                             </Button>
                         </Upload>
                     </Form.Item>
                     <Form.Item label="Description">
-                        <TextArea rows={2} allowClear />
+                        <TextArea rows={2} allowClear onChange={this.onChangeDesc} />
                     </Form.Item>
                     <Form.Item>
-                        <Button>Create Advert</Button>
+                        <Button onClick={() => console.log(this.state)}>Create Advert</Button>
                     </Form.Item>
                 </Form>
-            </Card>
+            </Card >
         );
     }
 }
