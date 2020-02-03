0c8202e535bc803c6477816809f8eb05051e0c77
bur-k <b.koc@etu.edu.tr>
2020-02-03 23:01:16 +0300

    integrate create-advert page with backend

--- viewmagnet-react/src/components/Advert/CreateAdvert.js
+++ viewmagnet-react/src/components/Advert/CreateAdvert.js
@@ -1,5 +1,7 @@
 import './CreateAdvert.css'
 import React from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux';
 import { Form, Input, Card, Radio, Select, Upload, Button, Icon } from 'antd'
 const { Option } = Select;
 const { TextArea } = Input;
@@ -8,16 +10,16 @@ class CreateAdvert extends React.Component {
     state = {
         title: "",
         description: "",
-        ageRange: "",
-        gender: "",
-        weather: "",
-        poster: "",
+        targetAge: "",
+        targetGender: "",
+        targetWeather: "",
+        // content: "",
     }
     onChangeTitle = str => this.setState({ title: str.target.value });
     onChangeDesc = str => this.setState({ description: str.target.value });
-    onChangeAgeRange = str => this.setState({ ageRange: str.target.value });
-    onChangeGender = str => this.setState({ gender: str.target.value });
-    onChangeWeather = str => this.setState({ weather: str });
+    onChangeAgeRange = str => this.setState({ targetAge: [str.target.value] });
+    onChangeGender = str => this.setState({ targetGender: [str.target.value] });
+    onChangeWeather = str => this.setState({ targetWeather: [str] });
 
     render() {
         return (
@@ -28,37 +30,37 @@ class CreateAdvert extends React.Component {
                     </Form.Item>
                     <Form.Item label="Target Age Range">
                         <Radio.Group onChange={this.onChangeAgeRange} >
-                            <Radio.Button value="BABY">Baby</Radio.Button>
-                            <Radio.Button value="CHILD">Child</Radio.Button>
-                            <Radio.Button value="YOUNG">Young</Radio.Button>
-                            <Radio.Button value="ADULT">Adult</Radio.Button>
-                            <Radio.Button value="ELDERLY">Elderly</Radio.Button>
+                            <Radio.Button value="0">Baby</Radio.Button>
+                            <Radio.Button value="1">Child</Radio.Button>
+                            <Radio.Button value="2">Young</Radio.Button>
+                            <Radio.Button value="3">Adult</Radio.Button>
+                            <Radio.Button value="4">Elderly</Radio.Button>
                         </Radio.Group>
                     </Form.Item>
                     <Form.Item label="Target Gender">
                         <Radio.Group onChange={this.onChangeGender}>
-                            <Radio.Button value="MALE">Male</Radio.Button>
-                            <Radio.Button value="FEMALE">Female</Radio.Button>
-                            <Radio.Button value="UNDETECTED">Undetected</Radio.Button>
+                            <Radio.Button value="0">Male</Radio.Button>
+                            <Radio.Button value="1">Female</Radio.Button>
+                            <Radio.Button value="2">Undetected</Radio.Button>
                         </Radio.Group>
                     </Form.Item>
                     <Form.Item label="Target Weather" >
                         <Select placeholder="Please select target weather" onChange={this.onChangeWeather}>
-                            <Option value="SUNNY">Sunny</Option>
-                            <Option value="CLOUDY">Cloudy</Option>
-                            <Option value="WINDY">Windy</Option>
-                            <Option value="FOGGY">Foggy</Option>
-                            <Option value="STORMY">Stormy</Option>
-                            <Option value="SNOWY">Snowy</Option>
-                            <Option value="RAINY">Rainy</Option>
-                            <Option value="UNKNOWN">Unknown</Option>
+                            <Option value="0">Sunny</Option>
+                            <Option value="1">Cloudy</Option>
+                            <Option value="2">Windy</Option>
+                            <Option value="3">Foggy</Option>
+                            <Option value="4">Stormy</Option>
+                            <Option value="5">Snowy</Option>
+                            <Option value="6">Rainy</Option>
+                            <Option value="7">Unknown</Option>
                         </Select>
                     </Form.Item>
                     <Form.Item label="Upload Advert Poster">
                         <Upload listType="picture" beforeUpload={(f) => {
                             let reader = new FileReader();
                             reader.readAsDataURL(f);
-                            reader.onloadend = () => { this.setState({ poster: reader.result }); return false; };
+                            //reader.onloadend = () => { this.setState({ content: reader }); return false; };
                         }}>
                             <Button>
                                 <Icon type="upload" />Click to upload
@@ -69,7 +71,7 @@ class CreateAdvert extends React.Component {
                         <TextArea rows={2} allowClear onChange={this.onChangeDesc} />
                     </Form.Item>
                     <Form.Item>
-                        <Button onClick={() => console.log(this.state)}>Create Advert</Button>
+                        <Button onClick={() => { console.log(this.props); axios.post("http://localhost:7000/api/ads", { ad: this.state }, { headers: { 'Authorization': this.props.token } }).then((res) => console.log(res)) }}>Create Advert</Button>
                     </Form.Item>
                 </Form>
             </Card >
@@ -77,4 +79,16 @@ class CreateAdvert extends React.Component {
     }
 }
 
-export default CreateAdvert;
+const mapStateToProps = state => {
+    return {
+        token: state.auth.token,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // onProfileUpdate: () => dispatch(logout())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateAdvert);
