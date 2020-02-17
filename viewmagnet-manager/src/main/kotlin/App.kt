df510094a27219eb5c8972a636cce3e4e3b939b1
berkayyildiz97 <berkay97yildiz@gmail.com>
2020-02-17 19:09:34 +0300

    Added map to manager

--- viewmagnet-manager/src/main/kotlin/App.kt
+++ viewmagnet-manager/src/main/kotlin/App.kt
@@ -1,1 +1,11 @@
-fun main() {}
+import io.javalin.Javalin
+
+fun main() {
+    val app = Javalin.create {
+        it.addStaticFiles("/public")
+    }.start(7000)
+
+    app.get("/billboard/:billboard_id", { ctx ->
+        ctx.html("Billboard " + ctx.pathParam("billboard_id") + " UP")
+    })
+}
