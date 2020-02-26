3a34e18b97e90482f91081c4d0544512eda88364
bur-k <b.koc@etu.edu.tr>
2020-02-26 23:14:51 +0300

    clear form after submit

--- viewmagnet-react/src/components/Advert/CreateAdvert.js
+++ viewmagnet-react/src/components/Advert/CreateAdvert.js
@@ -1,10 +1,11 @@
-import './CreateAdvert.css'
 import React from 'react'
 import axios from 'axios'
+import './CreateAdvert.css';
 import { connect } from 'react-redux';
 import { Form, Input, Card, Checkbox, Select, Upload, Button, Icon, message } from 'antd'
 const { Option } = Select;
 const { TextArea } = Input;
+const InputGroup = Input.Group;
 
 const ageOptions = [
     { label: 'Baby', value: '0' },
@@ -12,22 +13,26 @@ const ageOptions = [
     { label: 'Young', value: '2' },
     { label: 'Adult', value: '3' },
     { label: 'Elderly', value: '4' },
-  ];
+];
 
 const genderOptions = [
     { label: 'Male', value: '0' },
     { label: 'Female', value: '1' },
     { label: 'Undetected', value: '2' }
-  ];
+];
 
 class CreateAdvert extends React.Component {
     state = {
         title: '',
         description: '',
-        targetAge: '',
-        targetGender: '',
-        targetWeather: '',
+        targetAge: [],
+        targetGender: [],
+        targetWeather: [],
         content: '',
+        targetLowTemp: '',
+        targetHighTemp: '',
+        targetLowSoundLevel: '',
+        targetHighSoundLevel: '',
     }
 
     onChangeTitle = str => this.setState({ title: str.target.value });
@@ -35,22 +40,24 @@ class CreateAdvert extends React.Component {
     onChangeAgeRange = rangeSet => this.setState({ targetAge: rangeSet });
     onChangeGender = genderSet => this.setState({ targetGender: genderSet });
     onChangeWeather = weatherSet => this.setState({ targetWeather: weatherSet });
+    weatherFilterOption = (input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0;
 
     render() {
         return (
             <Card className="advert-card">
                 <Form>
                     <Form.Item label="Advert Title">
-                        <Input onChange={this.onChangeTitle} value={this.state.title} />
+                        <Input value={this.state.title} onChange={this.onChangeTitle} />
                     </Form.Item>
                     <Form.Item label="Target Age Range">
-                        <Checkbox.Group options={ageOptions} onChange={this.onChangeAgeRange} />
+                        <Checkbox.Group value={this.state.targetAge} options={ageOptions} onChange={this.onChangeAgeRange} />
                     </Form.Item>
                     <Form.Item label="Target Gender">
-                        <Checkbox.Group options={genderOptions} onChange={this.onChangeGender} />
+                        <Checkbox.Group value={this.state.targetGender} options={genderOptions} onChange={this.onChangeGender} />
                     </Form.Item>
                     <Form.Item label="Target Weather" >
-                        <Select placeholder="Please select target weather" mode="multiple" onChange={this.onChangeWeather}>
+                        <Select value={this.state.targetWeather}
+                            filterOption={this.weatherFilterOption} placeholder="Please select target weather" mode="multiple" onChange={this.onChangeWeather}>
                             <Option value="0">Sunny</Option>
                             <Option value="1">Cloudy</Option>
                             <Option value="2">Windy</Option>
@@ -75,15 +82,33 @@ class CreateAdvert extends React.Component {
                     <Form.Item label="Description">
                         <TextArea rows={2} allowClear onChange={this.onChangeDesc} value={this.state.description} />
                     </Form.Item>
+                    <Form.Item label="Target Temp Range">
+                        <InputGroup compact onChange={f => console.log(f)}>
+                            <Input style={{ width: 100, textAlign: 'center' }} value={this.state.targetLowTemp} type="number" placeholder="Minimum" onChange={val => this.setState({ targetLowTemp: val.target.value })} />
+                            <Input style={{ width: 30, borderLeft: 0, pointerEvents: 'none', backgroundColor: '#fff', }} placeholder="-" disabled />
+                            <Input style={{ width: 100, textAlign: 'center', borderLeft: 0 }} value={this.state.targetHighTemp} placeholder="Maximum" onChange={val => this.setState({ targetHighTemp: val.target.value })} />
+                        </InputGroup>
+                    </Form.Item>
+                    <Form.Item label="Target Sound Level Range">
+                        <InputGroup compact onChange={f => console.log(f)}>
+                            <Input style={{ width: 100, textAlign: 'center' }} value={this.state.targetLowSoundLevel} type="number" placeholder="Minimum" onChange={val => this.setState({ targetLowSoundLevel: val.target.value })} />
+                            <Input style={{ width: 30, borderLeft: 0, pointerEvents: 'none', backgroundColor: '#fff', }} placeholder="-" disabled />
+                            <Input style={{ width: 100, textAlign: 'center', borderLeft: 0 }} value={this.state.targetHighSoundLevel} placeholder="Maximum" onChange={val => this.setState({ targetHighSoundLevel: val.target.value })} />
+                        </InputGroup>
+                    </Form.Item>
                     <Form.Item>
-                        <Button onClick={() => {
+                        <Button type="primary" onClick={() => {
                             this.setState({
                                 title: '',
                                 description: '',
-                                targetAge: '',
-                                targetGender: '',
-                                targetWeather: '',
-                            }); axios.post("http://localhost:7000/api/ads", { ad: this.state }, { headers: { 'Authorization': localStorage.getItem('token') } }).then(() => message.success("Advert created succesfully!"))
+                                targetAge: [],
+                                targetGender: [],
+                                targetWeather: [],
+                                targetLowTemp: '',
+                                targetHighTemp: '',
+                                targetLowSoundLevel: '',
+                                targetHighSoundLevel: '',
+                            }); axios.post("http://localhost:7000/api/ads", { ad: this.state }, { headers: { 'Authorization': localStorage.getItem('token') } }).then(() => message.success("Advert created succesfully!")).catch(() => message.warning("Something went wrong!"))
                         }}>Create Advert</Button>
                     </Form.Item>
                 </Form>
