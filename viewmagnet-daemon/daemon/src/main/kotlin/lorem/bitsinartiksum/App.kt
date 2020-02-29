7791ec1152045961036a8171bc06a988e5ebac69
cakir-enes <cakir-enes@outlook.com>
2020-03-01 02:03:25 +0300

    add ad display, change resize support

--- viewmagnet-daemon/daemon/src/main/kotlin/lorem/bitsinartiksum/App.kt
+++ viewmagnet-daemon/daemon/src/main/kotlin/lorem/bitsinartiksum/App.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import lorem.bitsinartiksum.ad.AdManager
+import lorem.bitsinartiksum.ad.AdDisplay
 import model.BillboardEnvironment
 import model.Weather
 import java.util.*
@@ -13,7 +13,16 @@ import java.util.*
 fun main() = runBlocking<Unit> {
 
     //    val sp = StatusReporter(Config())
-    AdManager()
+    val display = AdDisplay(
+        AdDisplay.loadImg("https://images.unsplash.com/photo-1582996269871-dad1e4adbbc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80")!!,
+        200,
+        300
+    )
+    display.show()
+
+    delay(1000)
+    display.changeAd(AdDisplay.loadImg("https://images.unsplash.com/photo-1582740735409-d0ae8d48976e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80")!!)
+//    AdManager()
 
 //    val ts = TopicService.createFor(BillboardStatus::class.java, "env-listener", TopicContext())
 //    ts.subscribe {
