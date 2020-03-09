3e2c3d48ba4829e7188f6e2cc80be13a579a46bf
Alper Öner <alperoner0710@gmail.com>
2020-03-10 00:53:34 +0300

    object enum class added

--- viewmagnet-datamodel/src/main/kotlin/model/Billboard.kt
+++ viewmagnet-datamodel/src/main/kotlin/model/Billboard.kt
@@ -43,3 +43,27 @@ enum class Weather {
     CLEAR,
     CLOUDS;
 }
+
+enum class Object{
+    BACKGROUND,
+    AEROPLANE,
+    BICYCLE,
+    BIRD,
+    BOAT,
+    BOTTLE,
+    BUS,
+    CAR,
+    CAT,
+    CHAIR,
+    COW,
+    DININGTABLE,
+    DOG,
+    HORSE,
+    MOTORBIKE,
+    PERSON,
+    POTTEDPLANT,
+    SHEEP,
+    SOFA,
+    TRAIN,
+    TVMONITOR;
+}
