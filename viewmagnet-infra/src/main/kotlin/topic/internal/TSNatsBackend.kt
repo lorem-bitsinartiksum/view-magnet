c032af3bee0ec152633871a5e04632738a287397
cakir-enes <cakir-enes@outlook.com>
2020-03-25 19:41:56 +0300

    fix topic recv., only recv all and ind

--- viewmagnet-infra/src/main/kotlin/topic/internal/TSNatsBackend.kt
+++ viewmagnet-infra/src/main/kotlin/topic/internal/TSNatsBackend.kt
@@ -9,7 +9,7 @@ private val TopicContext.asSubject: String
         val country = if (this.country == Country.ALL) "*" else this.city.toString()
         val city = if (this.city == City.ALL) "*" else this.city.toString()
         val dist = if (this.district == District.ALL) "*" else this.city.toString()
-        val ind = if (this.individual == "ALL") "*" else "ALL"
+        val ind = individual
         return "$mode.$country.$city.$dist.$ind"
     }
 
@@ -29,12 +29,24 @@ internal class TSNatsBackend : TSBackend {
 
     override fun subscribe(name: String, context: TopicContext, consumer: (ByteArray) -> Unit): UnsubscribeToken {
 
-        val subject = "$name.${context.asSubject}"
-        val sub = nc.createDispatcher { consumer(it.data) }.subscribe(subject)
-        logger.atInfo().log("Subscribed to $subject")
+        val unsubTokens = mutableListOf<UnsubscribeToken>()
+        val natsSub = { subject: String ->
+            val sub = nc.createDispatcher {
+                consumer(it.data)
+            }.subscribe(subject)
+            unsubTokens.add({ sub.unsubscribe(subject) })
+        }
+
+        if (context.individual != "ALL") {
+            natsSub("${name}.${context.copy(individual = "ALL").asSubject}")
+        }
+
+        natsSub("${name}.${context.asSubject}")
+
+        logger.atInfo().log("Subscribed to $name")
         return {
             logger.atInfo().log("Unsubscribed from $context")
-            sub.unsubscribe(subject)
+            unsubTokens.forEach { it() }
         }
     }
 }
