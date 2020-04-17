e9f96662008e6fc8c30c151da706a8014e421700
bur-k <b.koc@etu.edu.tr>
2020-04-17 17:14:46 +0300

    update enum values

--- viewmagnet-react/src/components/Advert/advertOptions.js
+++ viewmagnet-react/src/components/Advert/advertOptions.js
@@ -6,17 +6,25 @@ export const ageOptions = [
     { label: 'ELDERLY', value: '4' },
 ];
 export const genderOptions = [
-    { label: 'MALE', value: '0' },
-    { label: 'FEMALE', value: '1' },
+    { label: 'MAN', value: '0' },
+    { label: 'WOMAN', value: '1' },
     { label: 'UNDETECTED', value: '2' }
 ];
 export const weatherOptions = [
-    { label: 'SUNNY', value: '0' },
-    { label: 'CLOUDY', value: '1' },
-    { label: 'WINDY', value: '2' },
-    { label: 'FOGGY', value: '3' },
-    { label: 'STORMY', value: '4' },
-    { label: 'SNOWY', value: '5' },
-    { label: 'RAINY', value: '6' },
-    { label: 'UNKNOWN', value: '7' }
+    { label: 'UNKNOWN', value: '0' },
+    { label: 'THUNDERSTORM', value: '1' },
+    { label: 'DRIZZLE', value: '2' },
+    { label: 'RAIN', value: '3' },
+    { label: 'SNOW', value: '4' },
+    { label: 'MIST', value: '5' },
+    { label: 'SMOKE', value: '6' },
+    { label: 'HAZE', value: '7' },
+    { label: 'DUST', value: '8' },
+    { label: 'FOG', value: '9' },
+    { label: 'SAND', value: '10' },
+    { label: 'ASH', value: '11' },
+    { label: 'SQUALL', value: '12' },
+    { label: 'TORNADO', value: '13' },
+    { label: 'CLEAR', value: '14' },
+    { label: 'CLOUDS', value: '15' }
 ];
